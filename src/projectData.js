578a1defd413409d95db81ab30573ba1f23448ce
Mradul999 <demorgans007@gmail.com>
2024-07-25 00:34:51 +0530

    image fix

--- src/projectData.js
+++ src/projectData.js
@@ -1,20 +1,20 @@
 export const projectData = [
   {
-    image: "../src/assets/chillflix.png",
+    image: "/chillflix.png",
     projectLink:"https://yourchillflix.netlify.app",
     title: "Chillflix",
     description:
       "Movies  website which allow users to play movies trailer,see ratings,cast   movies,search movies. It is build using ReactJs,Firebase,Tailwind CSS",
   },
   {
-    image: "../src/assets/nextblogs.png",
+    image: "/nextblogs.png",
     projectLink:"https://next-blogs-u170.onrender.com",
     title: "NextBlogs",
     description:
       "Bloging website with Admin  dashboard .It is build using React.js,Node.js,Tailwind CSS,MongoDB",
   },
   {
-    image: "../src/assets/portfolio.png",
+    image: "/portfolio.png",
     title: "Portfolio",
     description:
       "My Personal Portfolio Website made using React.js ,TailwindCSS and shadcn  ",
